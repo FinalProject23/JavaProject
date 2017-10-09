41bd8ab5eac44f44a73521b438c19ff1f1aa44ea
ChrisC0719 <31348437+ChrisC0719@users.noreply.github.com>
2017-10-09 10:01:19 -0500

    code

--- FinalProject/src/game/HangmanGame.java
+++ FinalProject/src/game/HangmanGame.java
@@ -4,20 +4,178 @@ import java.util.Scanner;
 import java.util.Random;
 
 
+
 /* This program is basically a hangman game but written in Java. Made for classwork/entertainment purposes only.
 // It will show the gallows, add a limb each time you get it wrong, and show you the letters that you have guessed. 
 Chooses a random word from the string array to use for the game */
-public class HangmanGame {
-
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		Random random = new Random();
-		//Create string array for words that are used in game. 
-		String [] words = {"royal", "heavy", "failure", "memes", "symbolic", "big"};
-		//Randomly select a word from the word array.
-		int word = random.nextInt(words.length);
+public class HangmanGame
+{
+	static Scanner input = new Scanner(System.in);
+	static Random random = new Random();
+	static char userGuess = 0;
+	//Create string array for words that are used in game. 
+	static String [] words = {"royal", "heavy", "scuzz", "memes", "dizzy", "abuzz"};
+	static char [] alaphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	static char [] guessedLetters = new char[26];
+	//Randomly select a word from the word array.
+	static int word = random.nextInt(words.length);
+	static String hiddenWord = words[word], newHiddenWord = null;
+	
+	public static void main(String[] args) 
+	{
 		//System.out.println("Welcome to Hangman!");
 		//System.out.println("Your word is " + words[word]);
+		display();
+		hangman();
+		//Replaces all the characters in the word with "-"s
+	
+		for(int x = 0; x < hiddenWord.length(); x++)
+		newHiddenWord = hiddenWord.replaceAll(".", "-");
+
+		System.out.println();
+		System.out.println(newHiddenWord);
+		System.out.println(" Welcome to hangmen! Press Y to play!");
+		userGuess = input.next().charAt(0);
+		System.out.println("Please guess a letter. The word is 5 letters long");
+		userGuess = input.next().charAt(0);
+		
+			
+		
+		
+		/* Replacing hidden characters with correct ones the user guesses. 
+		.indexOf detects to see if the character is in the string, if it is it returns a positive number, if not it returns a negative number.
+		We put in an if statement for when the user correctly guesses a letter in the word. */
+		
+			
+		
+		
+		if(words[word].indexOf(userGuess) != -1)
+		{
+			hiddenWord = words[word].replace('-', userGuess);
+			System.out.println("You guessed the letter correctly!");
+		}
+		else
+		{
+			System.out.println("Incorrect!");
+			System.out.println(newHiddenWord);
+			hangmanHead();
+			
+		}
+		
+	}
+	// Used to display hangman gallows
+	public static void hangman()
+	{
+		System.out.println("__________\r\n" + 
+				"|        |\r\n" + 
+				"|        |\r\n" + 
+				"|     -------\r\n" + 
+				"|	    \r\n" + 
+				"|	       \r\n" + 
+				"|     	     \r\n" + 
+				"|                  \r\n" + 
+				"|  	      \r\n" + 
+				"|_________\r\n" + 
+				"\r\n" + 
+				"");
+		
+		
+		
+	
+}
+	public static void hangmanHead()
+	{
+		System.out.println("__________\r\n" + 
+				"|        |\r\n" + 
+				"|        |\r\n" + 
+				"|     -------\r\n" + 
+				"|      (-_-)\r\n" + 
+				"|\r\n" + 
+				"|	       \r\n" + 
+				"|     	     \r\n" + 
+				"|                  \r\n" + 
+				"|  	      \r\n" + 
+				"|_________\r\n" + 
+				"\r\n" + 
+				"");
+		
+	}
+
+	
+	public static void hangmanRightArm()
+	{
+		System.out.println("__________\r\n" + 
+				"|        |\r\n" + 
+				"|        |\r\n" + 
+				"|     -------\r\n" + 
+				"|      (-_-)\r\n" + 
+				"|        |	\r\n" + 
+				"|      / | \r\n" + 
+				"|     	     \r\n" + 
+				"|                  \r\n" + 
+				"|  	      \r\n" + 
+				"|_________\r\n" + 
+				"\r\n" + 
+				"");
+		
+	}
+	
+	public static void hangmanLeftArm()
+	{
+		System.out.println("__________\r\n" + 
+				"|        |\r\n" + 
+				"|        |\r\n" + 
+				"|     -------\r\n" + 
+				"|      (-_-)\r\n" + 
+				"|        |	\r\n" + 
+				"|      / | \\\r\n" + 
+				"|     	     \r\n" + 
+				"|                  \r\n" + 
+				"|  	      \r\n" + 
+				"|_________\r\n" + 
+				"\r\n" + 
+				"");
+		
+	}
+	
+	public static void hangmanRightLeg()
+	{
+		System.out.println("__________\r\n" + 
+				"|        |\r\n" + 
+				"|        |\r\n" + 
+				"|     -------\r\n" + 
+				"|      (-_-)\r\n" + 
+				"|        |	\r\n" + 
+				"|      / | \\\r\n" + 
+				"|        |   \r\n" + 
+				"|      /   \r\n" + 
+				"|  	      \r\n" + 
+				"|_________\r\n" + 
+				"\r\n" + 
+				"");
+		
+	}
+	
+	public static void hangmanLeftLeg()
+	{
+		System.out.println("__________\r\n" + 
+				"|        |\r\n" + 
+				"|        |\r\n" + 
+				"|     -------\r\n" + 
+				"|      (-_-)\r\n" + 
+				"|        |	\r\n" + 
+				"|      / | \\\r\n" + 
+				"|        |   \r\n" + 
+				"|      /   \\ \r\n" + 
+				"|  	      \r\n" + 
+				"|_________\r\n" + 
+				"\r\n" + 
+				"");
+	}
+	
+	// Displays the intro to hangman, for style
+	public static void display()
+	{
 		System.out.println(" _                                             \r\n" + 
 				"| |                                            \r\n" + 
 				"| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  \r\n" + 
@@ -26,18 +184,11 @@ public class HangmanGame {
 				"|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|\r\n" + 
 				"                    __/ |                      \r\n" + 
 				"                   |___/                       ");
-		
-		hangman();
-		
-		
 	}
 	
-	// Used to display hangman gallows
-	public static void hangman()
+	public static void characterInWord()
 	{
 		
-		
-		
-	}
 
+}
 }
