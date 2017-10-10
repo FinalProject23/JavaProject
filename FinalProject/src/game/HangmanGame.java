489c4758c57b6503843cda84075782b23b38defa
Aaron Lang <blackthornlm@gmail.com>
2017-10-10 08:45:15 -0500

    if statements implemented

--- FinalProject/src/game/HangmanGame.java
+++ FinalProject/src/game/HangmanGame.java
@@ -14,18 +14,19 @@ public class HangmanGame {
 	static char userGuess = 0;
 	//Create string array for words that are used in game. 
 	static String [] words = {"royal", "heavy", "scuzz", "memes", "dizzy", "abuzz"};
-	static String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-	static char [] guessedLetters = new char[26];
+	//static String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+	static char[] guessedLetters = new char[26];
 	//Randomly select a word from the word array.
 	static int word = random.nextInt(words.length);
 	static String hiddenWord = words[word], newHiddenWord = null;
 	static String letterAt = null;
+	//Number of tries will be 6 so the hangman methods can properly execute.
+	static int numberOfTries = 6;
 	
 	public static void main(String[] args) {
-		//System.out.println("Welcome to Hangman!");
-		//System.out.println("Your word is " + words[word]);
 		display();
 		hangman();
+		boolean wordGuessed = false;
 		
 		//Replaces all the characters in the word with "-"s
 		for(int x = 0; x < hiddenWord.length(); x++)
@@ -37,6 +38,7 @@ public class HangmanGame {
 		do {
 		System.out.println("Guess a (lowercase plz) letter!");
 		userGuess = input.next().charAt(0);
+		
 		
 		/* Replacing hidden characters with correct ones the user guesses. 
 		.indexOf detects to see if the character is in the string, if it is it returns a positive number, if not it returns a negative number.
@@ -51,10 +53,48 @@ public class HangmanGame {
 		}
 		else
 		{
-			hangmanHead();
+			numberOfTries = numberOfTries - 1;
+			//If statements to display the other methods for gallows. Never repeats because the numberOfTries value always goes down by one,
+			//making the code move onto the next one each time. 
+			if(numberOfTries == 5)
+			{
+				hangmanHead();	
+			}
+
+			if(numberOfTries == 4)
+			{
+				hangmanRightArm();
+			}
+				
+			if(numberOfTries == 3)
+			{
+				hangmanLeftArm();
+			}
+					
+			if(numberOfTries == 2)
+			{	
+				hangmanRightLeg();
+			}
+			
+			if(numberOfTries == 1)
+			{
+				hangmanLeftLeg();
+				System.out.println("Oh, too bad! You lost! :(");
+				System.out.println("The actual word was " + hiddenWord);		
+			}	
+			
 			System.out.println(guessedLetters);
 			System.out.println(newHiddenWord);
 			System.out.println("Incorrect!");
+		}
+		
+		if(newHiddenWord.equals(hiddenWord))
+		{
+			wordGuessed = true;
+			if(wordGuessed)
+			{
+				victoryScreen();
+			}
 			
 		}
 	}while(!newHiddenWord.equals("-"));
@@ -62,6 +102,13 @@ public class HangmanGame {
 	}
 	
 	
+	private static void victoryScreen() {
+		System.out.println(hiddenWord);
+		System.out.println("Congratulations! You won!");
+		GameChooser.main(words);
+	}
+
+
 	// Used to display hangman gallows
 	public static void hangman()
 	{
@@ -176,13 +223,6 @@ public class HangmanGame {
 				"|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|\r\n" + 
 				"                    __/ |                      \r\n" + 
 				"                   |___/                       ");
-	}
-	
-	public static void characterInWord()
-	{
-		
-	        
-	   
 	}
 
 }
